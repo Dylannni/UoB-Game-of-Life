bb92302cdbd5cdf29cc89d1b820c35e5d74b4839
tommyd1123 <of23238@bristol.ac.uk>
2024-11-02 16:50:00 +0000

    change ip

--- distributed/client/gol/distributor.go
+++ distributed/client/gol/distributor.go
@@ -24,7 +24,7 @@ type distributorChannels struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-	brokerAdr := "34.229.127.162:8030"
+	brokerAdr := "54.197.205.145:8030"
 
 	client, err := rpc.Dial("tcp", brokerAdr)
 
