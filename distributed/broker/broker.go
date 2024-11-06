e65d94e682afef8623f0ca23f14df573c8ef25fb
Dylan Lin <dylanlin0411@gmail.com>
2024-11-06 22:45:19 +0000

    halo struct - v3.1

--- distributed/broker/broker.go
+++ distributed/broker/broker.go
@@ -72,7 +72,7 @@ func (b *Broker) RunGol(req *stdstruct.GameRequest, res *stdstruct.GameResponse)
 
 		slice := req.World[startY:endY]
 
-		err := server.Call("GameOfLife.Init", initReq{World: slice}, initRes{})
+		err := server.Call("GameOfLife.Init", stdstruct.initReq{World: slice}, stdstruct.initRes{})
 		if err != nil {
 			fmt.Println("Error init GameOfLife:", err)
 		}
