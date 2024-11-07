de72baf04554dd316a259a824b6019c848870276
Dylan Lin <dylanlin0411@gmail.com>
2024-11-07 17:09:59 +0000

    halo-parallel Done - v5

--- distributed/controller/controller.go
+++ distributed/controller/controller.go
@@ -203,39 +203,57 @@ func (s *GameOfLife) NextTurn(req *stdstruct.SliceRequest, res *stdstruct.SliceR
 
 	// res.Slice = mergeWorld
 
-	if height < 16 { 
-		// if image size is less than 16, don't do parallel (OVERKILL)
-		pieces := calculateNextState(req.StartY, req.EndY, req.StartX, req.EndX, extendworld, nextWorld)
-		mergeWorld = append(mergeWorld, pieces...)
 
-	} else { // image size bigger than 16x16
-		tempWorld := make([]chan [][]byte, s.threads)
-		for i := range tempWorld {
-			tempWorld[i] = make(chan [][]byte)
-		}
-
-		// heightPerThread := height / s.threads
-
-		heightPerThread := (height + s.threads - 1) / s.threads 
-
-		for i := 0; i < s.threads-1; i++ {
-			start := i * heightPerThread
-			end := start + heightPerThread
-			if end > height {
-				end = height
-			}
-			go worker(start, end, 0, req.EndX, extendworld, nextWorld, tempWorld[i]) 
-		}
-		go worker((s.threads-1)*heightPerThread, height, 0, req.EndX, extendworld, nextWorld, tempWorld[s.threads-1]) 
-	
-		// mergeWorld := make([][]byte, 0, height)
-		
-		for i := 0; i < s.threads; i++ {
-			pieces := <-tempWorld[i]
-			mergeWorld = append(mergeWorld, pieces...)
-		}
-
+	tempWorld := make([]chan [][]byte, s.threads)
+	for i := range tempWorld {
+		tempWorld[i] = make(chan [][]byte)
 	}
+
+	heightPerThread := (height + s.threads - 1) / s.threads 
+
+	for i := 0; i < s.threads-1; i++ {
+		start := i * heightPerThread
+		end := start + heightPerThread
+		if end > height {
+			end = height
+		}
+		go worker(start, end, 0, req.EndX, extendworld, nextWorld, tempWorld[i]) 
+	}
+	go worker((s.threads-1)*heightPerThread, height, 0, req.EndX, extendworld, nextWorld, tempWorld[s.threads-1]) 
+	
+	for i := 0; i < s.threads; i++ {
+		pieces := <-tempWorld[i]
+		mergeWorld = append(mergeWorld, pieces...)
+	}
+
+	// if height < 16 { 
+	// 	// if image size is less than 16, don't do parallel (OVERKILL)
+	// 	pieces := calculateNextState(req.StartY, req.EndY, req.StartX, req.EndX, extendworld, nextWorld)
+	// 	mergeWorld = append(mergeWorld, pieces...)
+
+	// } else { // image size bigger than 16x16
+	// 	tempWorld := make([]chan [][]byte, s.threads)
+	// 	for i := range tempWorld {
+	// 		tempWorld[i] = make(chan [][]byte)
+	// 	}
+
+	// 	heightPerThread := (height + s.threads - 1) / s.threads 
+
+	// 	for i := 0; i < s.threads-1; i++ {
+	// 		start := i * heightPerThread
+	// 		end := start + heightPerThread
+	// 		if end > height {
+	// 			end = height
+	// 		}
+	// 		go worker(start, end, 0, req.EndX, extendworld, nextWorld, tempWorld[i]) 
+	// 	}
+	// 	go worker((s.threads-1)*heightPerThread, height, 0, req.EndX, extendworld, nextWorld, tempWorld[s.threads-1]) 
+		
+	// 	for i := 0; i < s.threads; i++ {
+	// 		pieces := <-tempWorld[i]
+	// 		mergeWorld = append(mergeWorld, pieces...)
+	// 	}
+	// }
 
 	res.Slice = mergeWorld
 	return nil
