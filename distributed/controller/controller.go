fceae58e5f383f0a8573632a5caaae018f08e3f0
Dylan Lin <dylanlin0411@gmail.com>
2024-11-10 13:23:57 +0000

    SDL + Parallel v0.2

--- distributed/controller/controller.go
+++ distributed/controller/controller.go
@@ -91,7 +91,7 @@ func (s *GameOfLife) CalculateNextTurn(req *stdstruct.SliceRequest, res *stdstru
 	heightPerThread := height / req.Threads
 
 	var flippedCellsCh []chan []util.Cell // list of flipped cells channel that yet to merge
-	for i := 0; i < req.Threads-1; i++ {
+	for i := 0; i < req.Threads; i++ {
 		flippedCellCh := make(chan []util.Cell)
 		flippedCellsCh = append(flippedCellsCh, flippedCellCh)
 		if i == req.Threads-1 {
